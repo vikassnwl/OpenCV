8aa9e3caa690a571ab8802b86fb73a18114e09d7
Vikas Sanwal <vikassnwl@gmail.com>
2022-02-26 15:20:15 +0530

    update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,79 +3,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plott(img1, img2=None):
+def num_channels(img):
+    if len(img.shape) == 2:
+        return 1
+    return img.shape[-1]
+
+def is_colored(img):
+    return num_channels(img) == 3
+
+def is_grayescaled(img):
+    return num_channels(img) == 1
+
+def is_transparent(img):
+    return num_channels(img) == 4
+
+
+def plott(img1, img2=None, figsize=(6.4, 4.8)):
+    plt.figure(figsize=figsize)
+    
+    isColoredImg1 = is_colored(img1)
+    isGrayscaledImg1 = is_grayescaled(img1)
+    isTransparentImg1 = is_transparent(img1)
+
     if img2 is None:
         # plt.figure(figsize=(10, 10))
-        if len(img1.shape) == 3:
-            if img1.shape[-1] == 3:
-                plt.imshow(img1[..., ::-1])
-            else:
-                plt.imshow(np.concatenate((img1[..., 2::-1], img1[..., 3:]), axis=2))
-        else:
-            plt.imshow(img1, cmap="gray")
+        if isColoredImg1:
+            plt.imshow(img1[..., ::-1])
+        elif isGrayscaledImg1:
+            plt.imshow(img1, cmap='gray')
+        elif isTransparentImg1:
+            plt.imshow(np.concatenate((img1[..., 2::-1], img1[..., 3:]), axis=2))
     else:
         # plt.figure(figsize=(20, 20))
-        if (
-            len(img1.shape) == 3
-            and len(img2.shape) == 3
-            and img1.shape[-1] == 3
-            and img2.shape[-1] == 3
-        ):
+        isColoredImg2 = is_colored(img2)
+        isGrayscaledImg2 = is_grayescaled(img2)
+        isTransparentImg2 = is_transparent(img2)
+
+        if isColoredImg1 and isColoredImg2:
             plt.subplot(121)
             plt.imshow(img1[..., ::-1])
             plt.subplot(122)
             plt.imshow(img2[..., ::-1])
-        elif len(img1.shape) == 2 and len(img2.shape) == 2:
+        elif isGrayscaledImg1 and isGrayscaledImg2:
             plt.subplot(121)
             plt.imshow(img1, cmap="gray")
             plt.subplot(122)
             plt.imshow(img2, cmap="gray")
-        elif len(img1.shape) == 3 and img1.shape[-1] == 3 and len(img2.shape) == 2:
+        elif isColoredImg1 and isGrayscaledImg2:
             plt.subplot(121)
             plt.imshow(img1[..., ::-1])
             plt.subplot(122)
             plt.imshow(img2, cmap="gray")
-        elif len(img1.shape) == 2 and len(img2.shape) == 3 and img2.shape[-1] == 3:
+        elif isGrayscaledImg1 and isColoredImg2:
             plt.subplot(121)
             plt.imshow(img1, cmap="gray")
             plt.subplot(122)
             plt.imshow(img2[..., ::-1])
-        elif (
-            len(img1.shape) == 3
-            and len(img2.shape) == 3
-            and img1.shape[-1] == 4
-            and img2.shape[-1] == 4
-        ):
+        elif isTransparentImg1 and isTransparentImg2:
             plt.subplot(121)
             plt.imshow(np.concatenate((img1[..., 2::-1], img1[..., 3:]), axis=2))
             plt.subplot(122)
             plt.imshow(np.concatenate((img2[..., 2::-1], img2[..., 3:]), axis=2))
-        elif (
-            len(img1.shape) == 3
-            and img1.shape[-1] == 3
-            and len(img2.shape) == 3
-            and img2.shape[-1] == 4
-        ):
+        elif isColoredImg1 and isTransparentImg2:
             plt.subplot(121)
             plt.imshow(img1[..., ::-1])
             plt.subplot(122)
             plt.imshow(np.concatenate((img2[..., 2::-1], img2[..., 3:]), axis=2))
-        elif (
-            len(img1.shape) == 3
-            and img1.shape[-1] == 4
-            and len(img2.shape) == 3
-            and img2.shape[-1] == 3
-        ):
+        elif isTransparentImg1 and isColoredImg2:
             plt.subplot(121)
             plt.imshow(np.concatenate((img1[..., 2::-1], img1[..., 3:]), axis=2))
             plt.subplot(122)
             plt.imshow(img2[..., ::-1])
-        elif len(img1.shape) == 3 and img1.shape[-1] == 4 and len(img2.shape) == 2:
+        elif isTransparentImg1 and isGrayscaledImg2:
             plt.subplot(121)
             plt.imshow(np.concatenate((img1[..., 2::-1], img1[..., 3:]), axis=2))
             plt.subplot(122)
             plt.imshow(img2, cmap="gray")
-        elif len(img1.shape) == 2 and len(img2.shape) == 3 and img2.shape[-1] == 4:
+        elif isGrayscaledImg1 and isTransparentImg2:
             plt.subplot(121)
             plt.imshow(img1, cmap="gray")
             plt.subplot(122)
@@ -141,6 +145,6 @@ def erode(img, ksize=3, iters=1):
     return cv2.erode(img, kernel, iters)
 
 
-def morphologyEx(img, ksize=3, morph=0):
+def morphologyEx(img, morph=0, ksize=3):
     kernel = np.ones((ksize, ksize), np.uint8)
     return cv2.morphologyEx(img, cv2.MORPH_OPEN if morph==1 else cv2.MORPH_CLOSE, kernel)
